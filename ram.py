9c42ae2d7728c2ed0fa15a73957cb14964188cea
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-16 13:49:24 +0000

    Add files via upload

--- ram.py
+++ ram.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 paths=".//root//"
 r="/"
 def copys(s1:str,s2:str):
@@ -9,6 +10,23 @@ def copys(s1:str,s2:str):
     f2.write(s)
     f1.close()
     f2.close()
+def ppath():
+    print("------------------------------")
+    f1=open("/tmp/out.txt","r")
+    s=f1.read()
+    f1.close()
+    ss=s.split("\n")
+    for n in ss:
+        n=n.strip()
+        if n!="":
+            sss=n.split("(")
+            if sss[0].find("=>")<0:
+                print(sss[0])
+            else:
+                nn=sss[0].split("=>")
+                nnn=nn[1]
+                print(nnn)
+
 print("\033c\033[40;37m\ngive me a list file to create tree ? ")
 i=input()
 i=i.strip()
@@ -27,6 +45,8 @@ for f in fff:
         ff=f[1:]
         copys(r+ff,paths+ff)
         os.chmod(paths+ff,0o777)
+        os.system("ldd $1 > /tmp/out.txt".replace("$1",paths+ff))
+        ppath()
     else:
         os.mkdir(paths+f)
 ss="genisoimage -o disk.iso -input-charset utf-8 -b $1 -no-emul-boot -boot-load-size 4  -boot-info-table $2 ".replace("$2",paths).replace("$1",ii)
