4ce8fb14da974413be772f7773242514158498c7
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-16 09:26:47 +0000

    Add files via upload

--- ram.py
+++ ram.py
@@ -1,5 +1,14 @@
 import os
 paths=".//root//"
+r="/"
+def copys(s1:str,s2:str):
+    s=b""
+    f1=open(s1,"rb")
+    f2=open(s2,"wb")
+    s=f1.read()
+    f2.write(s)
+    f1.close()
+    f2.close()
 print("\033c\033[40;37m\ngive me a list file to create tree ? ")
 i=input()
 f1=open(i,"r")
@@ -8,4 +17,9 @@ f1.close()
 fff=ff.split(",")
 os.mkdir(paths)
 for f in fff:
-    os.mkdir(paths+f)
+    f=f.strip()
+    if f[0:1]=="!":
+        ff=f[1:]
+        copys(r+ff,paths+ff)
+    else:
+        os.mkdir(paths+f)
